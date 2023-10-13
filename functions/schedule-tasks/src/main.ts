73d913538e1c79b91c36f58b047c4c5ae55b06d5
nagyszem22 <takacsmarton22@gmail.com>
2023-10-14 00:05:12 +0100

    feat: weekend support feat: outside work hours support deprecate: before and after work hours deprecate: splitting tasks

--- functions/schedule-tasks/src/main.ts
+++ functions/schedule-tasks/src/main.ts
@@ -38,8 +38,8 @@ export type DayHours = {
 
 export enum ScheduleType {
   WORK_HOURS,
-  AFTER_WORK_HOURS,
-  BEFORE_WORK_HOURS,
+  OUTSIDE_WORK_HOURS,
+  WEEKEND,
   NONE
 }
 
@@ -49,8 +49,7 @@ export enum Urgency {
   THIS_WEEK,
   NEXT_WEEK,
   FIXED,
-  THIS_MONTH,
-  NONE
+  THIS_MONTH
 }
 
 export type TimeSlot = {
@@ -84,7 +83,6 @@ export default async ({ req, res, log, error }) => {
   const account = new Account(client);
   const databases = new Databases(client);
   try {
-    // @TODO add database and collection id to environment variables
     // @TODO: log that the scheduled tasks are being processed
     // if there is another task that is currenlty being processed, abort
 
@@ -103,110 +101,22 @@ export default async ({ req, res, log, error }) => {
     const tz: string = timeZone || DEFAULT_TIME_ZONE;
 
     // @TODO: validate tasks making sure that they can actually be scheduled and fit in the desired time slots
-    // a task can not be longer then the duration of the slot it is scheduled for if it can not be split
 
     const collection = await databases.listDocuments(adb['App'], adbc['Events'], [
-      Query.select(['$id', 'title', 'content', 'parentID', 'deadline', 'urgency', 'priority', 'scheduleType', 'duration', 'canSplit',
-        'isSplit', 'isDone', 'userID', 'start', 'end']),
+      Query.select(['$id', 'deadline', 'urgency', 'priority', 'scheduleType', 'duration', 'canSplit', 'start', 'end']),
       Query.equal('isDone', [ false ]),
+      Query.notEqual('urgency', [ 'NONE' ]),
     ]);
 
     const tasks: Task[] = Array.isArray(collection?.documents) ? collection.documents.map((document: any) => {
-      const task: Task = {
-        id: document.$id,
-        title: document.title,
-        content: document.content,
-        parentID: document.parentID,
-        deadline: DateTime.fromISO(document.deadline, { zone: DEFAULT_TIME_ZONE }),
-        urgency: (() => {
-          switch (document.urgency) {
-            case 'TODAY':
-              return Urgency.TODAY;
-            case 'TOMORROW':
-                return Urgency.TOMORROW;
-            case 'THIS_WEEK':
-                return Urgency.THIS_WEEK;
-            case 'NEXT_WEEK':
-                return Urgency.NEXT_WEEK;
-            case 'FIXED':
-                return Urgency.FIXED;
-            case 'THIS_MONTH':
-                return Urgency.THIS_MONTH;
-            case 'NONE':
-                return Urgency.NONE;
-          }
-        })(),
-        priority: document.priority,
-        scheduleType: (() => {
-          switch (document.scheduleType) {
-            case 'WORK_HOURS':
-              return ScheduleType.WORK_HOURS;
-            case 'AFTER_WORK_HOURS':
-              return ScheduleType.AFTER_WORK_HOURS;
-            case 'BEFORE_WORK_HOURS':
-              return ScheduleType.BEFORE_WORK_HOURS;
-            case 'NONE':
-              return ScheduleType.NONE;
-          }
-        })(),
-        duration: document.duration,
-        canSplit: document.canSplit,
-        isSplit: document.isSplit,
-        isDone: document.isDone,
-        userID: document.userID,
-        start: document.start ? DateTime.fromISO(document.start, { zone: DEFAULT_TIME_ZONE }) : undefined,
-        end: document.end ? DateTime.fromISO(document.end, { zone: DEFAULT_TIME_ZONE }) : undefined
-      };
+      const task: Task = mapDocumentToTask(document);
       return task;
-    }).filter(({ parentID }) => !parentID) : [];
+    }) : [];
 
-    const { create, update } = schedule(workHours, dayHours, tz, tasks);
-    
-    // delete all tasks that does have a parentID
-    const documentsWithParentIDs: any[] = Array.isArray(collection?.documents) ? collection.documents.filter(({ parentID }: any) => parentID) : [];
-    log({ documentsWithParentIDs });
-    const deletePromises = documentsWithParentIDs.map(({ $id }: any) => databases.deleteDocument('64d2c0765d92b052b3e0', '6501cbe36f6bb2db7c6f', $id));
-    await Promise.all(deletePromises);
-
-    // @TOOD: create the create and update methods
-    // make sure to wait one minute if more than 120
-    const createPromises = create.map(({ id, ...document }) => {
-      return databases.createDocument('64d2c0765d92b052b3e0', '6501cbe36f6bb2db7c6f', id, {
-        ...document,
-        urgency: (() => {
-          switch (document.urgency) {
-            case Urgency.TODAY:
-              return 'TODAY';
-            case Urgency.TOMORROW:
-              return 'TOMORROW';
-            case Urgency.THIS_WEEK:
-              return 'THIS_WEEK';
-            case Urgency.NEXT_WEEK:
-              return 'NEXT_WEEK';
-            case Urgency.FIXED:
-              return 'FIXED';
-            case Urgency.THIS_MONTH:
-              return 'THIS_MONTH';
-          }
-        })(),
-        scheduleType: (() => {
-          switch (document.scheduleType) {
-            case ScheduleType.WORK_HOURS:
-              return 'WORK_HOURS';
-            case ScheduleType.AFTER_WORK_HOURS:
-              return 'AFTER_WORK_HOURS';
-            case ScheduleType.BEFORE_WORK_HOURS:
-              return 'BEFORE_WORK_HOURS';
-            case ScheduleType.NONE:
-              return 'NONE';
-          }
-        })()
-      });
-    });
-    await Promise.all(createPromises);
+    const update = schedule(workHours, dayHours, tz, tasks);
 
     const updatePromises = update.map(({ id, start, end }) => {
-      return databases.updateDocument('64d2c0765d92b052b3e0', '6501cbe36f6bb2db7c6f', id, { start, end });
+      return databases.updateDocument(adb['App'], adbc['Events'], id, { start, end });
     });
     await Promise.all(updatePromises);
 
@@ -224,7 +134,58 @@ export default async ({ req, res, log, error }) => {
   }
 }
 
-export function schedule(workHours: WorkHours, dayHours: DayHours, timeZone: string, tasks: Task[], testDate: Date = null): UpsertTasks {
+function mapDocumentToTask(document: any): Task {
+  const task: Task = {
+    id: document.$id,
+    title: document.title,
+    content: document.content,
+    parentID: document.parentID,
+    deadline: DateTime.fromISO(document.deadline, { zone: DEFAULT_TIME_ZONE }),
+    urgency: (() => {
+      switch (document.urgency) {
+        case 'TODAY':
+          return Urgency.TODAY;
+        case 'TOMORROW':
+            return Urgency.TOMORROW;
+        case 'THIS_WEEK':
+            return Urgency.THIS_WEEK;
+        case 'NEXT_WEEK':
+            return Urgency.NEXT_WEEK;
+        case 'FIXED':
+            return Urgency.FIXED;
+        case 'THIS_MONTH':
+            return Urgency.THIS_MONTH;
+      }
+    })(),
+    priority: document.priority,
+    scheduleType: (() => {
+      switch (document.scheduleType) {
+        case 'WORK_HOURS':
+          return ScheduleType.WORK_HOURS;
+        case 'OUTSIDE_WORK_HOURS':
+          return ScheduleType.OUTSIDE_WORK_HOURS;
+        case 'BEFORE_WORK_HOURS':
+          return ScheduleType.OUTSIDE_WORK_HOURS;
+        case 'AFTER_WORK_HOURS':
+          return ScheduleType.OUTSIDE_WORK_HOURS;
+        case 'WEEKEND':
+          return ScheduleType.WEEKEND;
+        case 'NONE':
+          return ScheduleType.NONE;
+      }
+    })(),
+    duration: document.duration,
+    canSplit: document.canSplit,
+    isSplit: document.isSplit,
+    isDone: document.isDone,
+    userID: document.userID,
+    start: document.start ? DateTime.fromISO(document.start, { zone: DEFAULT_TIME_ZONE }) : undefined,
+    end: document.end ? DateTime.fromISO(document.end, { zone: DEFAULT_TIME_ZONE }) : undefined
+  };
+  return task;
+}
+
+export function schedule(workHours: WorkHours, dayHours: DayHours, timeZone: string, tasks: Task[], testDate: Date = null): Task[] {
   const now: DateTime = testDate ? DateTime.fromJSDate(testDate, { zone: timeZone }) : DateTime.now().setZone(timeZone);
   const originalTasks: Task[] = [ ...tasks.map(task => ({ ...task })) ];
 
@@ -252,32 +213,17 @@ export function schedule(workHours: WorkHours, dayHours: DayHours, timeZone: str
     const freeTimeSlots: TimeSlot[] = findFreeTimeSlots(scheduledTasks, timeZone, testDate);
 
     // schedule the current task and split it if necessary
-    const [ scheduledTask, remainderTask ]: Task[] = scheduleTask(workHours, dayHours, timeZone, freeTimeSlots, task);
+    const scheduledTask: Task = scheduleTask(workHours, dayHours, timeZone, freeTimeSlots, task);
 
     // add the scheduled task to the scheduled tasks
     scheduledTasks.push(scheduledTask);
-
-    // if there is a remainder, add it to the unscheduled tasks
-    if (remainderTask) {
-      unscheduledTasks.push(remainderTask);
-    }
   }
 
-  // CONTINUE HERE: CHECK IF THe NULL UPDATES WORK
-  const existingIDs: string[] = originalTasks.map(({ id }) => id);
-  const parentIDs: string[] = Array.from(new Set(originalTasks.map(({ parentID }) => parentID)));
-  const upsertTasks: UpsertTasks = {
-    create: scheduledTasks.filter(({ id }) => !existingIDs.includes(id)),
-    update: [
-      ...scheduledTasks.filter(({ id }) => !parentIDs.includes(id)).filter(({ id, start, end }) => {
-        const existingTask: Task = originalTasks.find(el => el.id === id);
-        return existingTask && (existingTask.start?.toMillis() !== start?.toMillis() || existingTask.end?.toMillis() !== end?.toMillis())
-      }),
-      ...originalTasks.filter(({ id }) => parentIDs.includes(id)).map((task) => ({ ...task, start: null, end: null }))
-    ]
-  };
-
-  return upsertTasks;
+  // CONTINUE HERE: CHECK IF THE NULL UPDATES WORK
+  return scheduledTasks.filter(item => {
+    const originalTask: Task = originalTasks.find(task => task.id === item.id);
+    return originalTask && (originalTask.start !== item.start || originalTask.end !== item.end);
+  });
 }
 
 export function findFreeTimeSlots(scheduledTasks: Task[], timeZone: string, testDate: Date = null): TimeSlot[] {
@@ -288,7 +234,9 @@ export function findFreeTimeSlots(scheduledTasks: Task[], timeZone: string, test
 
   const scheduledTasksCopy = [ ...scheduledTasks ];
 
-  scheduledTasksCopy.sort((a, b) => (a.start && b.start ? a.start.toMillis() - b.start.toMillis() : 0));
+  scheduledTasksCopy
+    .filter(item => item.start && item.end)
+    .sort((a, b) => (a.start && b.start ? a.start.toMillis() - b.start.toMillis() : 0));
 
   if (!scheduledTasksCopy || scheduledTasksCopy.length === 0) {
     return [{ start, end }];
@@ -317,14 +265,9 @@ export function findFreeTimeSlots(scheduledTasks: Task[], timeZone: string, test
 // schedule the current task
 // returns an array of two tasks: the scheduled task and a remainder task
 // if no splitting is necessary, the remainder is undefined
-export function scheduleTask(workHours: WorkHours, dayHours: DayHours, timeZone: string, freeTimeSlots: TimeSlot[], task: Task): Task[] {
-  const { duration: dur, canSplit, scheduleType } = task;
+export function scheduleTask(workHours: WorkHours, dayHours: DayHours, timeZone: string, freeTimeSlots: TimeSlot[], task: Task): Task {
+  const { duration: dur, scheduleType } = task;
   const duration: Duration = Duration.fromObject({ milliseconds: dur })
-
-  // @TODO: check if task can be scheduled
-  // if can not be split and is longer than the maximum allowed duration
-  // then save it as a task that can not be scheduled
-  // (otherwise the while loop will run forever)
 
   for (let i = 0; i < freeTimeSlots.length; i++) {
     const slot = freeTimeSlots[i];
@@ -333,22 +276,28 @@ export function scheduleTask(workHours: WorkHours, dayHours: DayHours, timeZone:
     for (let j = 0; j <= days; j++) {
       const today: DateTime = slot.start.plus({ days: j });
 
-      const startBeforeWork: DateTime = today.set({
+      let slotStart: DateTime = null;
+      let slotEnd: DateTime = null;
+
+      const startDay = today.set({
         hour: Math.floor(dayHours.start),
         minute: (dayHours.start - Math.floor(dayHours.start)) * 60,
         second: 0,
         millisecond: 0
       });
+      const endDay: DateTime = today.set({
+        hour: Math.floor(dayHours.end),
+        minute: (dayHours.end - Math.floor(dayHours.end)) * 60,
+        second: 0,
+        millisecond: 0
+      });
 
-      const endBeforeWork: DateTime = today.set({
+      const startWork: DateTime = today.set({
         hour: Math.floor(workHours.start),
         minute: (workHours.start - Math.floor(workHours.start)) * 60,
         second: 0,
         millisecond: 0
       });
-
-      const startWork: DateTime = endBeforeWork;
-
       const endWork: DateTime = today.set({
         hour: Math.floor(workHours.end),
         minute: (workHours.end - Math.floor(workHours.end)) * 60,
@@ -356,32 +305,51 @@ export function scheduleTask(workHours: WorkHours, dayHours: DayHours, timeZone:
         millisecond: 0
       });
 
-      const startAfterWork: DateTime = endWork;
+      // if weekend
+      if (today.weekday > 5) {
+        if (scheduleType === ScheduleType.WORK_HOURS) {
+          continue;
+        }
 
-      const endAfterWork: DateTime = today.set({
-        hour: Math.floor(dayHours.end),
-        minute: (dayHours.end - Math.floor(dayHours.end)) * 60,
-        second: 0,
-        millisecond: 0
-      });
-      
-      let slotStart: DateTime = null;
-      let slotEnd: DateTime = null;
-      if (scheduleType === ScheduleType.WORK_HOURS) {
-        slotStart = startWork > slot.start ? startWork : slot.start;
-        slotEnd = !slot.end || endWork < slot.end ? endWork : slot.end;
-      }
-      if (scheduleType === ScheduleType.BEFORE_WORK_HOURS) {
-        slotStart = startBeforeWork > slot.start ? startBeforeWork : slot.start;
-        slotEnd = !slot.end || endBeforeWork < slot.end ? endBeforeWork : slot.end;
-      }
-      if (scheduleType === ScheduleType.AFTER_WORK_HOURS) {
-        slotStart = startAfterWork > slot.start ? startAfterWork : slot.start;
-        slotEnd = !slot.end || endAfterWork < slot.end ? endAfterWork : slot.end;
-      }
-      if (scheduleType === ScheduleType.NONE) {
-        slotStart = startBeforeWork > slot.start ? startBeforeWork : slot.start;
-        slotEnd = !slot.end || endAfterWork < slot.end ? endAfterWork : slot.end;
+        slotStart = startDay > slot.start ? startDay : slot.start;
+        slotEnd = !slot.end || endDay < slot.end ? endDay : slot.end;
+
+
+      // if weekday
+      } else {
+        if (scheduleType === ScheduleType.WEEKEND) {
+          continue;
+        }
+
+        if (scheduleType === ScheduleType.WORK_HOURS) {
+          slotStart = startWork > slot.start ? startWork : slot.start;
+          slotEnd = !slot.end || endWork < slot.end ? endWork : slot.end;
+        }
+
+        if (scheduleType === ScheduleType.NONE) {
+          slotStart = startDay > slot.start ? startDay : slot.start;
+          slotEnd = !slot.end || endDay < slot.end ? endDay : slot.end;
+        }
+
+        if (scheduleType === ScheduleType.OUTSIDE_WORK_HOURS) {
+          if (slot.start < startDay) {
+            slotStart = startDay;
+          } else if (slot.start < startWork) {
+            slotStart = slot.start;
+          } else if (slot.start < endWork) {
+            slotStart = endWork;
+          } else {
+            slotStart = slot.start < endDay ? slot.start : endDay;
+          }
+
+          if (slot.end < startWork) {
+            slotEnd = slot.end;
+          } else if (slot.end < endWork) {
+            slotEnd = startWork;
+          } else {
+            slotEnd = !slot.end || slot.end > endDay ? endDay : slot.end;
+          }
+        }
       }
 
       const slotDuration: Duration = slotEnd.diff(slotStart);
@@ -390,27 +358,10 @@ export function scheduleTask(workHours: WorkHours, dayHours: DayHours, timeZone:
         task.start = slotStart;
         task.end = slotStart.plus(duration);
 
-        return [task, null ];
-      }
-
-      if (canSplit && duration > slotDuration && slotDuration.as('hours') >= MIN_SPLIT_DURATION) {
-        const newTask: Task = {
-          ...task,
-          id: uuidv4(),
-          parentID: task.id,
-          duration: slotDuration.as('milliseconds'),
-          start: slotStart,
-          end: slotEnd 
-        };
-        const remainderTask: Task = {
-          ...task,
-          id: uuidv4(),
-          parentID: task.parentID || task.id,
-          duration: duration.as('milliseconds') - slotDuration.as('milliseconds'),
-        };
-
-        return [ newTask, remainderTask ];
+        return task;
       }
     }
   }
+
+  return task;
 }
